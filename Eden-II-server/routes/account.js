4e3214cdfee24448c0e09eef7752b2418907dafe
Dexter Tong <dyt217@nyu.edu>
2016-10-29 05:35:39 -0400

    removed passport

--- Eden-II-server/routes/account.js
+++ Eden-II-server/routes/account.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function(req, res) {
    res.send('<html><body><form method="post"><input type="text" name="username"></form></body></html>');
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res, next) {
-    res.send(req.user);
+router.post('/login', function(req, res, next) {
+    console.log(req.body);
 });
 
 module.exports = router;
