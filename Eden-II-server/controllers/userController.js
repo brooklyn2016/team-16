7bd0424c56214bed7640c5f36b8c3b1e4ef53415
Kevin Naughton Jr <kdn251@nyu.edu>
2016-10-29 07:19:07 -0400

    added presentation

--- Eden-II-server/controllers/userController.js
+++ Eden-II-server/controllers/userController.js
@@ -47,7 +47,16 @@ module.exports = {
      * userController.create()
      */
     create: function (req, res) {
-        var user = new userModel({			username : req.body.username,			password : req.body.password,			firstName : req.body.firstName,			lastName : req.body.lastName,			phone : req.body.phone,			parents : req.body.parents,			participants : req.body.participants,			caretakers : req.body.caretakers,			groupHome : req.body.groupHome
+        var user = new userModel({
+			username : req.body.username,
+			password : req.body.password,
+			firstName : req.body.firstName,
+			lastName : req.body.lastName,
+			phone : req.body.phone,
+			parents : req.body.parents,
+			participants : req.body.participants,
+			caretakers : req.body.caretakers,
+			groupHome : req.body.groupHome
         });
 
         user.save(function (err, user) {
@@ -79,7 +88,16 @@ module.exports = {
                 });
             }
 
-            user.username = req.body.username ? req.body.username : user.username;			user.password = req.body.password ? req.body.password : user.password;			user.firstName = req.body.firstName ? req.body.firstName : user.firstName;			user.lastName = req.body.lastName ? req.body.lastName : user.lastName;			user.phone = req.body.phone ? req.body.phone : user.phone;			user.parents = req.body.parents ? req.body.parents : user.parents;			user.participants = req.body.participants ? req.body.participants : user.participants;			user.caretakers = req.body.caretakers ? req.body.caretakers : user.caretakers;			user.groupHome = req.body.groupHome ? req.body.groupHome : user.groupHome;			
+            user.username = req.body.username ? req.body.username : user.username;
+			user.password = req.body.password ? req.body.password : user.password;
+			user.firstName = req.body.firstName ? req.body.firstName : user.firstName;
+			user.lastName = req.body.lastName ? req.body.lastName : user.lastName;
+			user.phone = req.body.phone ? req.body.phone : user.phone;
+			user.parents = req.body.parents ? req.body.parents : user.parents;
+			user.participants = req.body.participants ? req.body.participants : user.participants;
+			user.caretakers = req.body.caretakers ? req.body.caretakers : user.caretakers;
+			user.groupHome = req.body.groupHome ? req.body.groupHome : user.groupHome;
+			
             user.save(function (err, user) {
                 if (err) {
                     return res.status(500).json({
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Ian
 Engineered an application for Eden II in order to decipher and recognize autistic individual's speech.
 
 There may or may not be an instance of this running [here](http://ec2-54-165-13-70.compute-1.amazonaws.com:3000/)
+
+You can find our final presentation of our application [here](https://docs.google.com/presentation/d/18MQRt_VWbF8fx8Z14gquqIph4c72xy_UfhZlAnLklkc/edit?ts=58147da0#slide=id.p)
